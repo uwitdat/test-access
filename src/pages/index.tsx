6f43f5fbf521029cf5220be53e492561b7b7e876
uwitdat <ben.shekhtman@myplanet.com>
2023-08-30 10:11:33 -0400

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,10 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <img src="https://picsum.photos/id/237/536/354" />
+      <img
+        src="https://picsum.photos/id/237/536/354"
+        style={{ height: '5px' }}
+      />
     </main>
   );
 }
